18b1c17cbdd538377cf606ce78be421cdb0417be
F3rNnd4 <fernandaalves.tds2.senai@gmail.com>
2025-10-09 16:29:11 -0300

    feat: implement home screen and tabs layout with navigation

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,34 @@
-import { Slot } from 'expo-router';
-import { AuthProvider } from '../contexts/AuthContext';
+import { Tabs } from "expo-router";
+import { Text } from "react-native";
 
-export default function RootLayout() {
-    return (
-        <AuthProvider>
-            <Slot />
-        </AuthProvider>
-    );
+export default function TabsLayout() {
+  return (
+    <Tabs
+        screenOptions={{
+            headerShown: false,
+            TabsBarStyle: {
+                backgroundColor: '#fff',
+                borderTopColor: '#888',
+                borderTopWidth: 1,
+            },
+            tabBarActiveTintColor: '#007aff',
+            tabBarInactiveTintColor: '#888',
+        }}
+    >
+        <Tabs.Screen
+            name="home"
+            options={{
+                title: "Home",
+                tabBarIcon: ({ color }) => <Text style={{ fontSize: 24 }}>🏠</Text>,
+            }}
+        />
+        <Tabs.Screen
+            name="profile"
+            options={{
+                title: "Perfil",
+                tabBarIcon: ({ color }) => <Text style={{ fontSize: 24 }}>👤</Text>,
+            }}
+        />
+    </Tabs>
+  );
 }
